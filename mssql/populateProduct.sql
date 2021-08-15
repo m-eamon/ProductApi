5c9281e66ab8ea95de25323dcd6f87d200e88183
meamon <eamon.muldowney@gmail.com>
2021-08-15 17:02:04 +0100

    Adding sql script to popluate Product table

---
+++ mssql/populateProduct.sql
@@ -0,0 +1,19 @@
+use ProductsDB;
+
+insert into Product
+values("When Harry Met Sally","Romantic Comedy","Billy Crystal and Meg Ryan star in the quirky romantic comedy directed by Rob Reiner", 
+7.99M, "HMV");
+
+insert into Product 
+values("Ghostbusters", "Comedy", "1984 American supernatural comedy film directed byIvan Reitman and written by Dan Aykroyd and Harold Ramis", 
+8.99M, "Tower Records");               
+
+insert into Product
+values("Ghostbusters 2","Comedy", "Sequel to the supernatural comedy film directed byIvan Reitman and written by Dan Aykroyd and Harold Ramis", 
+9.99M, "Tower Records");
+                       
+insert into Product
+values("Rio Bravo", "Western", "1959 American Western film produced and directed by Howard Hawks and starring John Wayne, Dean Martin and Ricky Nelson", 
+3.99M, "HMV");
+                    
+                        
